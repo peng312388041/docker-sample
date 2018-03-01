1e01ddd1c690a46e814d75fd38f97e9d2b511613
pengchong <pengchong@192.168.204.136>
2018-03-01 11:08:24 +0800

    test

--- src/main/java/io/vertx/sample/hello/HelloVerticle.java
+++ src/main/java/io/vertx/sample/hello/HelloVerticle.java
@@ -7,7 +7,7 @@ public class HelloVerticle extends AbstractVerticle {
   @Override
   public void start() throws Exception {
     vertx.createHttpServer().requestHandler(request -> {
-      request.response().end("Hello Java world-haaa !");
+      request.response().end("Hello Java world-ha !");
     }).listen(8080);
   }
 }
